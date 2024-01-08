586a8c36ed558d6f6a4ec23c049ddaf4d6dd0703
et120 <e.trotter120@gmail.com>
2024-01-08 10:34:35 -0800

    controller and route

--- Controllers/SayHello.cs
+++ Controllers/SayHello.cs
@@ -1,6 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace BoronTrotterESayHelloEndpoint.Controllers;
 
-public class SayHello
+[ApiController]
+[Route("[controller]")]
+public class SayHelloController : ControllerBase
 {
+    public string greeting = "";
 
+    public SayHelloController()
+    {
+        greeting = "Hello, ";
+    }
+
+    [HttpPost]
+    [Route("AddName/{userName}")]
+    public string AddName(string userName)
+    {
+        return greeting + userName;
+    }
 }
